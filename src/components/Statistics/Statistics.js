e847a3c646ec146066c44801b3574114228ba6b2
Kilivnik <ekstasy1990@gmail.com>
2022-08-22 23:23:18 +0300

    deleting extra files

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import s from './Statistics.module.css';
 
 export default function Statistics(props) {
-  // const { good, neutral, bad, total, positivePercentage } = props;
 
   function propNameNormalize(string) {
     return (
@@ -22,12 +21,6 @@ export default function Statistics(props) {
           {propNameNormalize(propName)}: {props[propName]}
         </li>
       ))}
-
-      {/* <li className={s.item}>Good: {good}</li>
-      <li className={s.item}>Neutral: {neutral}</li>
-      <li className={s.item}>Bad: {bad}</li>
-      <li className={s.item}>Total: {total}</li>
-      <li className={s.item}>Positive percentage: {positivePercentage}</li> */}
     </ul>
   );
 }
